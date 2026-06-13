3caf7fd19f4d52fd4038dbd711baece96597804c
Bayi <bayi@bayi.hu>
2026-06-14 01:09:03 +0200

    Remoev GeOre unmoveable tag

--- pack/kubejs/server_scripts/tags.js
+++ pack/kubejs/server_scripts/tags.js
@@ -2,4 +2,5 @@ ServerEvents.tags('block', event => {
   const buddingBlocks = /geore:budding_.*/
   event.remove('create:non_movable', buddingBlocks)
   event.remove('c:relocation_not_supported', buddingBlocks)
+  event.remove('c:non_movable', buddingBlocks)
 })
